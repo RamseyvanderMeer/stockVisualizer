00514330818834aea0bf24ca9b458c7762c3565e
RamseyvanderMeer <ramvandermeer@gmail.com>
2022-11-19 13:32:54 -0600

    get data dynamically

--- pages/api/getSummary.js
+++ pages/api/getSummary.js
@@ -5,14 +5,11 @@ import axios from 'axios';
 export default async function handler(req, res) {
   try {
     const response = await axios.get(
-      'https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=5T64BD51IKDOO1MV',
+      `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${req.query.ticker}&apikey=5T64BD51IKDOO1MV`,
       { json: true, headers: { 'User-Agent': 'request' } }
     );
-    // console.log(response.data)
     return res.status(200).json(response.data)
   } catch (err) {
-    // Handle Error Here
     console.log(err);
-    
   }
 };
--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,41 @@ import Head from 'next/head';
 import { useState, useEffect } from 'react';
 
 export default function Home() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState({})
+  const [dataLoaded, setDataLoaded] = useState(false)
+  const [ticker, setTicker] = useState("")
 
   const fetchData = async () => {
-    const response = await fetch('/api/hello');
-    const tickerData = await response.json();
+    const response = await fetch(`api/getSummary?ticker=${ticker}`)
+    const tickerData = await response.json()
     setData(tickerData);
+    setDataLoaded(true);
   };
 
-  useEffect(() => {
-    try {
-      fetchData();
-      console.log(data);
-    } catch (err) {
-      console.log('err');
-    }
-  }, []);
-
-  const handleSubmit = async () => {
-    alert(`submit`)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    alert(`submit`);
+    fetchData();
+    console.log(data);
   };
+
+  const handleChangeInput = (e) => {
+    setTicker(e.target.value)
+  }
 
   return (
     <div>
-      <h1>{`stock: ${data.Symbol}`}</h1>
+      {dataLoaded ? (
+        <h1>{`stock: ${data.Symbol}`}</h1>
+      ) : (
+        <h1> input stock ticker </h1>
+      )}
       <form onSubmit={handleSubmit}>
-        <label htmlFor="first">First Name</label>
-        <input type="text" id="first" name="first" required />
+        <label htmlFor="ticker">Ticker</label>
+        <input onChange={handleChangeInput} placeholder="aapl" type="text" id="ticker" required />
         <button type="submit">Submit</button>
       </form>
+      
     </div>
   );
 }
